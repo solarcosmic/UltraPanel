88d8626cafba4a52697c8961872b49b9f055ed05
Solar <abe@solarcosmic.net>
2026-03-04 19:05:27 +1300

    List of Containers, Active Ones & Express (v1.0.0)

--- index.js
+++ index.js
@@ -4,9 +4,37 @@ const Docker = require("dockerode");
 var docker = new Docker();
 
 const app = express();
+app.set("view engine", "ejs");
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port, () => {
     console.log(`Running on 127.0.0.1:${port}`);
+    listContainers();
 });
+
+app.get('/', async (req, res) => {
+    // From https://stackoverflow.com/a/57710329
+    //res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    //res.header('Expires', '-1');
+    //res.header('Pragma', 'no-cache');
+    const containers = await listContainers();
+    console.log("Container List: " + JSON.stringify(containers, null, 2));
+    res.render('panel', {
+        active_containers: containers
+    });
+});
+
+async function listContainers() {
+    try {
+        const containers = await docker.listContainers({all: true});
+        const containerIds = containers.map(ctInfo => {
+            //return ctInfo.Names[0].substring(1) || ctInfo.Id;
+            return ctInfo;
+        });
+        return containerIds;
+    } catch (error) {
+        console.log("Error occurred: " + containers);
+        return [];
+    }
+}
